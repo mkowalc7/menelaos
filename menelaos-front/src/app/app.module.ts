d8bbcba36b5cd55f5b5b05d33075f9f392fca263
Michał Kowalczyk <mkowalczyk1904@gmail.com>
2018-12-30 21:27:58 +0100

    MEN-23 Added welcome page

--- menelaos-front/src/app/app.module.ts
+++ menelaos-front/src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    RegisterComponent,
+    LoginComponent,
+    AboutComponent,
   ],
   imports: [
     BrowserModule,
