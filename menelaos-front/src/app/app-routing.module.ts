daf5b3f4a6fc797ca0a9488eb96a6b6cc6527aa1
Michał Kowalczyk <mkowalczyk1904@gmail.com>
2018-12-30 21:27:58 +0100

    MEN-23 Added welcome page

--- menelaos-front/src/app/app-routing.module.ts
+++ menelaos-front/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomePageComponent } from "./welcome-page/welcome-page.component";
+import { RegisterComponent } from "./register/register.component";
+import { LoginComponent } from "./login/login.component";
+import { AboutComponent } from "./about/about.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomePageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
